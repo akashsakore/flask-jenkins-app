3b50b6659e49f217fccfe14094619fd2255078ac
Ubuntu <ubuntu@ip-172-31-83-21.ec2.internal>
2025-06-28 09:22:27 +0000

    update msg to test ci/cd

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🚀 Hello from Flask CI/CD pipeline!"
+    return "🚀 Hello from Flask CI/CD pipeline! Its a first trigger from github"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
